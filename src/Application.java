6b3249c18e0a03941af2d095ee4a482d1abe1d93
alpanajotov <77166578+alpanajotov@users.noreply.github.com>
2022-05-16 15:45:47 +0300

    Implement operation restriction on Trader level

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Application {
 
     public static void main(String[] args) {
@@ -17,17 +15,32 @@ public class Application {
 
             int operationId = Terminal.input();
 
-            if (operationId == 2) {
+            if (operationId == Trader.OPERATION_START_DAY) {
+                traderManager.startDay();
+            }
+
+            boolean isProcessBlocked = traderManager.isOperationNotProcessable() && Trader.isOperationRestricted(operationId);
+
+            if (Trader.isOperationRestricted(operationId)) {
+                if(isProcessBlocked) {
+                    System.out.println("Трябва да си отворите деня, за да изберете тази операция");
+                    continue;
+                }
+            }
+
+            if (operationId == Trader.OPERATION_BUY) {
                 traderManager.buy(10);
             }
 
-            if (operationId == 3) {
+            if (operationId == Trader.OPERATION_SELL) {
                 traderManager.sell(15);;
             }
 
-            if (operationId == 4) {
+            if (operationId == Trader.OPERATION_REPORT) {
                 double result = traderManager.getAccountAmount();
+                Terminal.output("=================");
                 Terminal.output(result);
+                Terminal.output("=================");
             }
         }
     }
--- src/Trader.java
+++ src/Trader.java
@@ -1,6 +1,18 @@
 public class Trader {
 
+    public static final int OPERATION_START_DAY = 1;
+    public static final int OPERATION_BUY       = 2;
+    public static final int OPERATION_SELL      = 3;
+    public static final int OPERATION_REPORT    = 4;
+    public static final int OPERATION_END_DAY   = 5;
+
     public int accountAmount = 0;
+    public boolean isActive = false;
+
+    public static boolean isOperationRestricted(int operationId) {
+        return operationId == Trader.OPERATION_BUY || operationId == Trader.OPERATION_SELL || operationId == Trader.OPERATION_REPORT;
+
+    }
 
     public void buy(double price) {
         accountAmount -= price;
@@ -12,5 +24,17 @@ public class Trader {
 
     public double getAccountAmount() {
         return accountAmount;
+    }
+
+    public boolean isOperationProcessable() {
+        return isActive;
+    }
+
+    public boolean isOperationNotProcessable() {
+        return !isOperationProcessable();
+    }
+
+    public void startDay() {
+        isActive = true;
     }
 }
